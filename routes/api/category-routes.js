3e29a8c78a4e5b21dbb338eea795c3c07b51ddde
MIRANDA <miranda.zermeno4@gmail.com>
2021-11-06 00:00:26 -0500

    completed POST route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,18 +4,48 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const catergoryData = await Category.findAll({
+      include: [
+        {model: Product, attributes:['id', 'product_name', 'price', 'stock', 'category_id']}
+      ],
+    });
+    res.status(200).json(catergoryData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const oneCatData = await Category.findByPk(req.params.id,{
+      include: [
+        {model: Product, attributes:['id', 'product_name', 'price', 'stock', 'category_id']}
+      ],
+    });
+    res.status(200).json(oneCatData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try{
+    const catergoryData = await Category.findByPk(req.params.id,{
+      include: [
+        {model: Product, attributes:['id', 'product_name', 'price', 'stock', 'category_id']}
+      ],
+    });
+    res.status(200).json(oneCatData);
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
