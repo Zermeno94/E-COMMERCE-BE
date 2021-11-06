ba19d9bf3b7c33369f4ae54f138e6f138442511b
MIRANDA <miranda.zermeno4@gmail.com>
2021-11-05 23:43:40 -0500

    defined columns in products.js

--- models/Product.js
+++ models/Product.js
@@ -21,7 +21,7 @@ Product.init(
   // product_name
  {
    product_name:{
-    type: DataTypes.INTEGER,
+    type: DataTypes.STRING,
     allowNull: false,
    }
   
@@ -29,17 +29,37 @@ Product.init(
 
  {
    //price
-  
+  price:{
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate:{
+      isDecimal: true,
+    }
+  }
  },
 
  {
 
  // stock
+ stock:{
+   type: DataTypes.INTEGER,
+   allowNull: false,
+   defaultValue: 10,
+   validate: {
+     isNumeric: true,
+   }
+ }
 
  },
 
 {
   // category_id
+  category_id:{
+    references:{
+      model:'category',
+      key: 'key'
+    }
+  }
 },
   
   {
