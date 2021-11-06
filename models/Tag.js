95d50cc06ce84d2948210cc133fc333622e6859a
MIRANDA <miranda.zermeno4@gmail.com>
2021-11-05 23:18:56 -0500

    added comments and some code to models/tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,17 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey: true,
+      autoIncrement: true,
+    }
   },
+
+  //tag_name
+
+
   {
     sequelize,
     timestamps: false,
